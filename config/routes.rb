0f86a53c05f52ea23e73e3eb583163b5dc6b5396
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2018-01-08 17:33:48 -0500

    API created

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  resources :registered_applications, only: [:index, :new, :create, :show, :destroy]
 
   devise_for :users
-  resources :users
 
   get "welcome/index"
   get 'welcome/about'
+  root 'welcome#index'
 
-  root to: 'welcome#index'
+  resources :users
+  resources :registered_applications, only: [:index, :new, :create, :show, :destroy]
+
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+     resources :events, only: [:create]
+   end
 
 end
